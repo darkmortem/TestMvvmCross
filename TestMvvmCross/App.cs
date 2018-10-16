ab065d57c5e1fa108f961bec6a38baa53d84f257
Marcos Jose Perez Perez <marcperez@deloitte.com>
2018-10-16 17:39:54 -0300

    Initial check-in of module TestMvvmCross

---
+++ TestMvvmCross/App.cs
@@ -0,0 +1,19 @@
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Platform.IoC;
+
+namespace TestMvvmCross
+{
+    public class App : MvvmCross.Core.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<ViewModels.MainViewModel>());
+        }
+    }
+}
---
+++ TestMvvmCross/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TestMvvmCross
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
